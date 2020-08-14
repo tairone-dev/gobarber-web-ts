4f8d6808f4d686e32da25307f13f4ba8b7304a50
Tairone Livinalli <tairone.livinalli@softplan.com.br>
2020-08-14 08:29:35 -0300

    Improvements in Auth Context

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,15 +1,64 @@
-import React, { createContext } from 'react';
+import React, { createContext, useCallback, useState, useContext } from 'react';
+import api from '../services/api';
+
+interface AuthState {
+  token: string;
+  user: Record<string, unknown>;
+}
+
+interface SignInCredentials {
+  email: string;
+  password: string;
+}
 
 interface AuthContextData {
-  name: string;
+  user: Record<string, unknown>;
+  signIn(credentials: SignInCredentials): Promise<void>;
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
-export const AuthProvider: React.FC = ({ children }) => {
+const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState(() => {
+    const token = localStorage.getItem('@GoBarber:token');
+    const user = localStorage.getItem('@GoBarber:user');
+
+    if (token && user) {
+      return { token, user: JSON.parse(user) };
+    }
+
+    return {} as AuthState;
+  });
+
+  const signIn = useCallback(async ({ email, password }) => {
+    const response = await api.post('sessions', {
+      email,
+      password,
+    });
+
+    const { token, user } = response.data;
+
+    localStorage.setItem('@GoBarber:token', token);
+    localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+    setData({ token, user });
+  }, []);
+
   return (
-    <AuthContext.Provider value={{ name: 'Tairone' }}>
+    <AuthContext.Provider value={{ user: data.user, signIn }}>
       {children}
     </AuthContext.Provider>
   );
 };
+
+function useAuth(): AuthContextData {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+
+  return context;
+}
+
+export { AuthProvider, useAuth };
