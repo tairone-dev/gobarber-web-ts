79c6823a806a22099cd0b555dab890782e6b6cd5
Tairone Livinalli <tairone.livinalli@softplan.com.br>
2020-09-15 20:51:51 -0300

    Added appointments static list

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { FiPower, FiClock } from 'react-icons/fi';
 import {
@@ -9,6 +9,8 @@ import {
   Content,
   Schedule,
   NextAppointment,
+  Section,
+  Appointment,
   Calendar,
 } from './styles';
 
@@ -17,6 +19,8 @@ import { useAuth } from '../../hooks/AuthContext';
 import logoImg from '../../assets/logo.svg';
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
   const { signOut, user } = useAuth();
 
   return (
@@ -65,6 +69,60 @@ const Dashboard: React.FC = () => {
               </span>
             </div>
           </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars1.githubusercontent.com/u/17730232?s=460&u=9017dc28c6d94e59b900b716a8570cc6e04d520a&v=4"
+                  alt="Tairone Livinalli"
+                />
+
+                <strong>Tairone Livinalli</strong>
+              </div>
+            </Appointment>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars1.githubusercontent.com/u/17730232?s=460&u=9017dc28c6d94e59b900b716a8570cc6e04d520a&v=4"
+                  alt="Tairone Livinalli"
+                />
+
+                <strong>Tairone Livinalli</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Tarde</strong>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars1.githubusercontent.com/u/17730232?s=460&u=9017dc28c6d94e59b900b716a8570cc6e04d520a&v=4"
+                  alt="Tairone Livinalli"
+                />
+
+                <strong>Tairone Livinalli</strong>
+              </div>
+            </Appointment>
+          </Section>
         </Schedule>
         <Calendar />
       </Content>
